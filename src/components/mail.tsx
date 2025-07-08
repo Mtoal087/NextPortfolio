f3493f1a2c8c11dd1fc1f9d15d8889d5d8f41b4c
Matt Toal <mdtnz3@umsystem.edu>
2025-07-07 20:40:54 -0500

    Mail

---
+++ src/components/mail.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Mail = () => {
+    return (
+        <div className='pt-14 pb-40'>
+            <h2 className='text-4xl md:text-5xl font-bold mb-10'>Interested in working together?</h2>
+            <a href="mailto:mtoal087@gmail.com" className="cursor-pointer font-bold whitespace-nowrap mt-6 px-8 py-3 text-white border-2 rounded-full border-white hover-bg-secondary hover-border-secondary transition-colors">Get in Touch</a>
+        </div>
+    )
+}
+
+export default Mail
