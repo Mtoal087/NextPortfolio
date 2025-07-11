63a0400fa075d5e04e8eee4d857e326c7f4c059f
Matt Toal <mdtnz3@umsystem.edu>
2025-07-11 15:37:58 -0500

    Added animation to hero

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-Change <a> -> <Link> and <img /> -> <Image />
-
 This is a [Next.js](https://nextjs.org) project bootstrapped with [`create-next-app`](https://nextjs.org/docs/app/api-reference/cli/create-next-app).
 
 ## Getting Started
--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const HeroElement = () => {
     return (
-        <div className='relative heroElem w-full pt-20 pb-40 m-auto flex justify-center text-center flex-col items-center z-1'>
+        <div className='relative heroElem w-full pt-20 pb-40 m-auto flex justify-center text-center flex-col items-center z-1 hero-animate'>
             <p className='text-xl mb-5'>Hi, I'm Matthew Toal.</p>
             <h1 className='heroTitle inline-block max-w-2xl lg:max-w-4xl w-auto relative text-5xl md:text-6xl lg:text-7xl tracking-tighter mb-10 font-bold heroShinyBg'>
                 I'm a <span className='text-secondary'>software engineer</span> that enjoys <span className='text-secondary'>learning</span> and <span className='text-secondary'>developing</span> tools to <span className='text-secondary'>help</span> others.
